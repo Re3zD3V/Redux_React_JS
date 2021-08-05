801198a581979f5934d25b01062a8733e5dcbb0d
Re3z <ghiz.k.dev@gmail.com>
2021-08-05 02:49:17 +0200

    Section 2.3_Reducers: done. Created state: initialState and reducers: reducer dealing with BUY_PHONE and default actions

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// STATE
+const initialState = {
+	phones : 5,
+	laptops : 12,
+	tvs : 8
+};
+
+
 // ACTION
 const BUY_PHONE = 'BUY_PHONE';
 
@@ -6,4 +14,23 @@ function buyPhone(quantity = 1) {
 		type: BUY_PHONE,
 		quantity
 	};
+}
+
+
+// REDUCER
+// ( prevState, action ) => newState
+
+const reducer = ( prevState = initialState, action ) => {
+	switch( action.type ) {
+		case BUY_PHONE:
+			if( prevState.phones >= action.quantity ) {
+				return {
+					...prevState,
+					phones : prevState.phones - action.quantity
+				};
+			}
+			return prevState;
+		default:
+			return prevState;
+	}
 }
