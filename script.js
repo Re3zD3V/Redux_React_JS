784e20a7d856b381b920ae5841dcf05e9bbb8ab1
Re3z <ghiz.k.dev@gmail.com>
2021-08-08 03:50:44 +0200

    Section 2.5_Handle multi actions: Added tablet image, modified .container css to add margin-bottom, added tablets to initial state, created action and action creator buyTablet, added BUY_TABLET case to reducer, initialized count-tablet value on redux store creation, dispatched buyTablet action on buy-tablet button click event

--- script.js
+++ script.js
@@ -2,16 +2,24 @@
 const initialState = {
 	phones : 5,
 	laptops : 12,
-	tvs : 8
+	tablets : 8
 };
 
 
 // ACTION
 const BUY_PHONE = 'BUY_PHONE';
+const BUY_TABLET = 'BUY_TABLET';
 
 function buyPhone(quantity = 1) {
 	return {
 		type: BUY_PHONE,
+		quantity
+	};
+}
+
+function buyTablet(quantity = 1) {
+	return {
+		type: BUY_TABLET,
 		quantity
 	};
 }
@@ -30,6 +38,14 @@ const reducer = ( prevState = initialState, action ) => {
 				};
 			}
 			return prevState;
+		case BUY_TABLET:
+			if( prevState.tablets >= action.quantity ) {
+				return {
+					...prevState,
+					tablets : prevState.tablets - action.quantity
+				};
+			}
+			return prevState;
 		default:
 			return prevState;
 	}
@@ -38,17 +54,26 @@ const reducer = ( prevState = initialState, action ) => {
 // CREATE STORE
 const store = Redux.createStore(reducer);
 
-// Initialize number of phones on store creation
-const availablePhones = document.getElementById('count');
+// Initialize number of phones and tablets on store creation
+const availablePhones = document.getElementById('count-phones');
 availablePhones.innerHTML = store.getState().phones;
 
-// Set buy-phone button onClick action
+const availableTablets = document.getElementById('count-tablets');
+availableTablets.innerHTML = store.getState().tablets;
+
+// Set buy-phone and buy-tablet button onClick action
 const buyPhoneBtn = document.getElementById('buy-phone');
 buyPhoneBtn.addEventListener('click', () => {
 	store.dispatch(buyPhone());
 });
 
+const buyTabletBtn = document.getElementById('buy-tablet');
+buyTabletBtn.addEventListener('click', () => {
+	store.dispatch(buyTablet());
+});
+
 // Subscrire to every store state change
 store.subscribe(() => {
 	availablePhones.innerHTML = store.getState().phones;
+	availableTablets.innerHTML = store.getState().tablets;
 });
