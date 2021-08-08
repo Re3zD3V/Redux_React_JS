d29753bf9e3ff14908c8ae6b1decb83aa1733734
Re3z <ghiz.k.dev@gmail.com>
2021-08-08 03:05:10 +0200

    Working on buying phones using quantity: everything ok except quantity elements style on the page (needs to be fixed

--- script.js
+++ script.js
@@ -38,17 +38,31 @@ const reducer = ( prevState = initialState, action ) => {
 // CREATE STORE
 const store = Redux.createStore(reducer);
 
+// Display initial state from redux store
+// console.log(store.getState());
+
+// Function to dispatch buyPhone action on button click, then display new redux store state
+// const buy = () => {
+// 	store.dispatch(buyPhone(3));
+// 	console.log(store.getState());
+// }
+
 // Initialize number of phones on store creation
 const availablePhones = document.getElementById('count');
 availablePhones.innerHTML = store.getState().phones;
 
 // Set buy-phone button onClick action
+// const quantityPhones = document.getElementById('quantity');
 const buyPhoneBtn = document.getElementById('buy-phone');
+
 buyPhoneBtn.addEventListener('click', () => {
+	// let quantity = quantityPhones.value;
+	// store.dispatch(buyPhone(quantity));
 	store.dispatch(buyPhone());
 });
 
 // Subscrire to every store state change
 store.subscribe(() => {
 	availablePhones.innerHTML = store.getState().phones;
+	// console.log(store.getState());
 });
