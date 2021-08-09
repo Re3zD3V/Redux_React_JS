8af9007a981b49b62e5eeb58e373a2fc00fdc1c0
Re3z <ghiz.k.dev@gmail.com>
2021-08-09 03:11:54 +0200

    Section 2.6_Combine reducers: Added tv image, added tvs to initial state, created action and action creator buyTv, added BUY_TV case to original reducer, initialized count-tvs value on redux store creation, dispatched buyTv action on buy-tv button click event

--- script.js
+++ script.js
@@ -2,13 +2,15 @@
 const initialState = {
 	phones : 5,
 	laptops : 12,
-	tablets : 8
+	tablets : 8,
+	tvs: 15
 };
 
 
 // ACTION
 const BUY_PHONE = 'BUY_PHONE';
 const BUY_TABLET = 'BUY_TABLET';
+const BUY_TV = 'BUY_TV';
 
 function buyPhone(quantity = 1) {
 	return {
@@ -20,6 +22,13 @@ function buyPhone(quantity = 1) {
 function buyTablet(quantity = 1) {
 	return {
 		type: BUY_TABLET,
+		quantity
+	};
+}
+
+function buyTv(quantity = 1) {
+	return {
+		type: BUY_TV,
 		quantity
 	};
 }
@@ -46,6 +55,14 @@ const reducer = ( prevState = initialState, action ) => {
 				};
 			}
 			return prevState;
+		case BUY_TV:
+			if( prevState.tvs >= action.quantity ) {
+				return {
+					...prevState,
+					tvs : prevState.tvs - action.quantity
+				};
+			}
+			return prevState;
 		default:
 			return prevState;
 	}
@@ -61,6 +78,9 @@ availablePhones.innerHTML = store.getState().phones;
 const availableTablets = document.getElementById('count-tablets');
 availableTablets.innerHTML = store.getState().tablets;
 
+const availableTvs = document.getElementById('count-tvs');
+availableTvs.innerHTML = store.getState().tvs;
+
 // Set buy-phone and buy-tablet button onClick action
 const buyPhoneBtn = document.getElementById('buy-phone');
 buyPhoneBtn.addEventListener('click', () => {
@@ -72,8 +92,14 @@ buyTabletBtn.addEventListener('click', () => {
 	store.dispatch(buyTablet());
 });
 
+const buyTvBtn = document.getElementById('buy-tv');
+buyTvBtn.addEventListener('click', () => {
+	store.dispatch(buyTv());
+});
+
 // Subscrire to every store state change
 store.subscribe(() => {
 	availablePhones.innerHTML = store.getState().phones;
 	availableTablets.innerHTML = store.getState().tablets;
+	availableTvs.innerHTML = store.getState().tvs;
 });
